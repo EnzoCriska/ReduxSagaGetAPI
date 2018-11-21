dbb20ea8dd85d476677e4eaab638f8bfc72caeae
EnzoCriska <TieuGiaCat97@gmail.com>
2018-11-21 11:12:40 +0700

    getMoviesFromApi not work

--- src/sagas/movieSagas.js
+++ src/sagas/movieSagas.js
@@ -1,10 +1,10 @@
 import { put, takeLatest } from "redux-saga/effects";
-import { API } from "./API";
+import { Api } from "./API";
 import { FETCH_SUCCESS, FETCH_FAILED, FETCH_MOVIE } from "../actions/actionTypes";
 
 function* fetchMovies(){
     try {
-        const receiverMovies = yield API.getMoviesFromAPI();
+        const receiverMovies = yield Api.getMoviesFromApi();
         yield put({type: FETCH_SUCCESS, receiverMovies : receiverMovies});
     } catch (error) {
         yield put({type: FETCH_FAILED, error})
